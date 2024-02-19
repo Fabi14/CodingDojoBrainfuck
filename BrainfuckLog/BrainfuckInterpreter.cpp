e6a58ea395782c99a6c00b0257d15784b2a617da
CppFabi <fabian22191@googlemail.com>
2024-02-19 20:10:18 +0100

    Refactor commands into map

--- BrainfuckLog/BrainfuckInterpreter.cpp
+++ BrainfuckLog/BrainfuckInterpreter.cpp
@@ -1,36 +1,40 @@
 #include "pch.h"
 #include "BrainfuckInterpreter.h"
+#include <map>
+#include <functional>
 
 auto BrainfuckInterpreter::run(std::string const& code) -> Memory
 {
     Memory memory;
     auto memoryPointer{ memory.begin() };
+
+    std::map<char, std::function<void()>> commands
+    {
+        {'+', [&] {(*memoryPointer)++; }},
+        {'-', [&] {(*memoryPointer)--; }},
+        {'>', [&]
+            {
+                memoryPointer++;
+                if (memoryPointer == memory.end())
+                {
+                    memoryPointer = memory.begin();
+                }
+            }
+        },
+        {'<', [&]
+            {
+                if (memoryPointer == memory.begin())
+                {
+                    memoryPointer = memory.end();
+                }
+                memoryPointer--;
+            }
+        }
+    };
+
     for (const auto c : code)
     {
-        if (c == '+')
-        {
-            (*memoryPointer)++;
-        }
-        else if (c == '-')
-        {
-            (*memoryPointer)--;
-        }
-        else if (c == '>')
-        {
-            memoryPointer++;
-            if (memoryPointer == memory.end())
-            {
-                memoryPointer = memory.begin();
-            }
-        }
-        else if (c == '<')
-        {
-            if (memoryPointer == memory.begin())
-            {
-                memoryPointer = memory.end();
-            }
-            memoryPointer--;
-        }
+        std::invoke(commands.at(c));
     }
     return memory;
 }
