12ad286fe546d35693c11cb6320e557d1867d2ca
CppFabi <fabian22191@googlemail.com>
2024-02-19 18:26:07 +0100

    RED: Test for empty Programm

--- UnitTestBrainfuck/UnitTestBrainfuck.cpp
+++ UnitTestBrainfuck/UnitTestBrainfuck.cpp
@@ -9,8 +9,13 @@ namespace UnitTestBrainfuck
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestEmptyMemoryForNoCode)
 		{
+			std::string codeToExecute{ "" };
+			BrainfuckInterpreter::Memory memory{ BrainfuckInterpreter::run(codeToExecute) };
+
+			BrainfuckInterpreter::Memory expectedMemoryState{};
+			Assert::AreEqual(expectedMemoryState, memory);
 		}
 	};
 }
