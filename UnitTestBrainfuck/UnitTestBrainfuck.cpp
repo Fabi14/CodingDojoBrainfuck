b61324adf60ee1778b09f478a1f5ca4aa539af6e
CppFabi <fabian22191@googlemail.com>
2024-02-19 19:44:30 +0100

    RED: TestIncrementTheDataPointerAndIncrement

--- UnitTestBrainfuck/UnitTestBrainfuck.cpp
+++ UnitTestBrainfuck/UnitTestBrainfuck.cpp
@@ -33,5 +33,13 @@ namespace UnitTestBrainfuck
 
 			Assert::AreEqual(static_cast<unsigned char>(255u), memory.get().front());
 		}
+
+		TEST_METHOD(TestIncrementTheDataPointerAndIncrement)
+		{
+			std::string codeToExecute{ ">+" };
+			BrainfuckInterpreter::Memory memory{ BrainfuckInterpreter::run(codeToExecute) };
+
+			Assert::AreEqual(static_cast<unsigned char>(1u), *std::next(memory.get().begin()));
+		}
 	};
 }
