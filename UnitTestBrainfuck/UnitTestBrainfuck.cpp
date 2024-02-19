d95f0649102b0eade76cd4cdb8f6e0f635ffe28c
CppFabi <fabian22191@googlemail.com>
2024-02-19 18:14:38 +0100

    Add project files.

---
+++ UnitTestBrainfuck/UnitTestBrainfuck.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestBrainfuck
+{
+	TEST_CLASS(UnitTestBrainfuck)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
